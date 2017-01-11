1b844ab745e5ae85e11f37105611d4294c96fd2d
ry-tk-br <rybrdg@gmail.com>
2017-01-11 18:48:14 +0900

    nununununununu

--- app/models/search/technique.rb
+++ app/models/search/technique.rb
@@ -10,6 +10,12 @@
   def matches
     t = ::Technique.arel_table
     results = ::Technique.all
+
+    results = results.where(purpose1: true) if value_to_boolean(purpose1)
+    results = results.where(purpose2: true) if value_to_boolean(purpose2)
+    results = results.where(purpose3: true) if value_to_boolean(purpose3)
+    results = results.where(purpose4: true) if value_to_boolean(purpose4)
+
     #results = results.where(contains(t[:categories_id], categories_id)) if categories_id.present?
     #results = results.where("categories_id = 1")# if categories_id.present?
     #results = results.where(t[:categories_id] == categories_id) if categories_id.present?
@@ -19,6 +25,6 @@
     #results = results.where(contains(t[:text1], text) | contains(t[:text2], text)) if text.present?
 
     ##results = results.where(t[:categories_id].matches(categories_id)) if categories_id.present?
-    ##results
+    results
   end
 end
